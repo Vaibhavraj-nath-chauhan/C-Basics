322a17be4d5453c5a5d9e03e36d86a4f8ab60957
Vaibhavraj Nath Chauhan <vaibhavrajbanna@gmail.com>
2022-07-21 21:50:42 +0530

    6

---
+++ 6. Printf Return type.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+int main(){
+	int a =printf("Hello World");  //Return type of printf is int and it return the total number of successful prints on counsole screen 
+	printf("-> %d",a);
+}
